06e95e208f35f25c24cfb2a5f1c7e15fccbf252e
Samuel Carvalho <samuel.carvalho.dev@gmail.com>
2023-08-11 14:03:20 -0300

    primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Olá e bem-vindo!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+
+        System.out.println("Concluí a aula 01 e agora estou mergulhando em Java!");
+    }
+}
